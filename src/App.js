fe4c378a89486baff8c123a52dcfc4b19e1ba176
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-02-03 21:27:12 -0600

    98% finished project

--- src/App.js
+++ src/App.js
@@ -1,57 +1,95 @@
 import Footer from "./components/Footer.jsx";
 import Nav from "./components/Nav.jsx";
-import './index.css'
-import { BrowserRouter as Router, Route } from "react-router-dom/cjs/react-router-dom.min.js";
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min.js";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
-import { books } from './data'
+import { books } from "./data";
 import BookInfo from "./pages/BookInfo.jsx";
 import Cart from "./pages/Cart.jsx";
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-  function addToCart (book) {
-    setCart([...cart, book])
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
   }
 
   useEffect(() => {
-    console.log(cart)
-  }, [cart])
+    console.log(cart);
+  }, [cart]);
 
-// function addToCart(book) {
-//   const dupeItem = cart.find(item => +item.id === +book.id)
-//   if (dupeItem) {
-//     setCart(cart.map((item) => {
-//       if (item.id === dupeItem.id) {
-//         return {
-//           ...item,
-//           quantity: item.quantity + 1
-//         }
-//       } else {
-//         return item
-//       }
-//     })
-//     )
-//   } else {
-//     setCart([...cart, {...book, quantity: 1}])
-//   }
-  
-// }
+  // function addToCart(book) {
+  //   const dupeItem = cart.find(item => +item.id === +book.id)
+  //   if (dupeItem) {
+  //     setCart(cart.map((item) => {
+  //       if (item.id === dupeItem.id) {
+  //         return {
+  //           ...item,
+  //           quantity: item.quantity + 1
+  //         }
+  //       } else {
+  //         return item
+  //       }
+  //     })
+  //     )
+  //   } else {
+  //     setCart([...cart, {...book, quantity: 1}])
+  //   }
 
-// useEffect(() => {
-//   console.log(cart)
-// }, [cart])
+  // }
+
+  // useEffect(() => {
+  //   console.log(cart)
+  // }, [cart])
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()}/>
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
-        <Route path="/cart" exact render={() => <Cart books={books}/>} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
+        <Route
+          path="/cart"
+          exact
+          render={() => <Cart books={books} />}
+          cart={cart}
+          changeQuantity={changeQuantity}
+          removeItem={removeItem}
+        />
         <Footer />
       </div>
     </Router>
